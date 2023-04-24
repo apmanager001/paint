03ffa57456660ac78440b3d65a0d8d56ff1139d9
apamanger <rfoley00@live.com>
2023-04-23 22:17:29 -0400

    second commit, added shapes and updated header

--- js/index.js
+++ js/index.js
@@ -1,5 +1,23 @@
+// shapes
+const canvas = document.querySelector("canvas");
+const context = canvas.getContext("2d")
+
+//rectangle, (horizontal, vertical, width, height)
+context.fillStyle = "blue"
+context.fillRect(150, 15, 100, 50)
+// square
+context.fillStyle = "red";
+context.fillRect(10, 100, 50, 50)
+//triangle with lines
+context.beginPath();
+context.moveTo(50, 10);
+context.lineTo(10, 70);
+context.lineTo(90, 70);
+context.fill()
 
 
+
+//function for shapes from mdn
 function draw() {
     const canvas = document.getElementById("canvas");
     if (canvas.getContext) {
@@ -11,4 +29,3 @@ function draw() {
     }
   }
 
-  
